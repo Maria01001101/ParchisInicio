56dbfa29360e4d5ef6efdc6341781ff52f49130b
Maria01001101 <maria.fuentesmazutiel@ucr.ac.cr>
2025-10-16 11:33:23 -0600

    Primer Commit

---
+++ src/main/java/main/ParchisInicio.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main;
+
+import view.GUIInicio;
+
+/**
+ *
+ * @author maria
+ */
+public class ParchisInicio {
+
+    public static void main(String[] args) {
+        GUIInicio inicio = new GUIInicio();
+        inicio.setVisible(true);
+        inicio.setLocationRelativeTo(null);
+    }
+}
